1b60c303c77a11c1cdb12f2641f67fe3a900f6fd
antons201 <penugin2012@yandex.ru>
2020-07-10 14:15:22 +1000

    Добавьте файлы проекта.

---
+++ AppNew/AppNew/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppNew
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnValueChanged(object slider, ValueChangedEventArgs args)
+        {
+            if (slider == redSlider)
+            {
+                redLabel.Text = String.Format("Red = {0}", (int)args.NewValue);
+            }
+            else if (slider == greenSlider)
+            {
+                greenLabel.Text = String.Format("Green = {0}", (int)args.NewValue);
+            }
+            else if (slider == blueSlider)
+            {
+                blueLabel.Text = String.Format("Blue = {0}", (int)args.NewValue);
+            }
+            else if (slider == brightnessSlider)
+            {
+                brightnessLabel.Text = String.Format("Brightness = {0}", (int)args.NewValue);
+            }
+
+            boxView.Color = Color.FromRgba((int)redSlider.Value, (int)greenSlider.Value, 
+                (int)blueSlider.Value, (int)brightnessSlider.Value);
+        }
+    }
+}
